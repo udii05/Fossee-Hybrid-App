93b9b6fdf2b0e28984250a7bf430bb6a69ae7d6d
Udita Chakraborty <udita.chakraborty05@gmail.com>
2026-01-28 20:21:22 +0530

    update files

--- backend/server/manage.py
+++ backend/server/manage.py
@@ -1,15 +1,9 @@
-#!/usr/bin/env python
 import os
 import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django."
-        ) from exc
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
