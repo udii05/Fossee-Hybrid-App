d3b4ae0b32081392b3f6fb54d018a83718dcdc37
Udita Chakraborty <udita.chakraborty05@gmail.com>
2026-01-28 20:31:48 +0530

    update models

--- backend/server/api/models.py
+++ backend/server/api/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+class Dataset(models.Model):
+    filename = models.CharField(max_length=255)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    total_equipment = models.IntegerField()
+    avg_pressure = models.FloatField()
+    avg_temperature = models.FloatField()
+
+    def __str__(self):
+        return self.filename
